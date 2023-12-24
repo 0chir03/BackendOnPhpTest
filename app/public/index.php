2bb4318d20d0d71ba3ec6a482a04ee281a739640
ochir <@0chir03>
2023-12-24 21:29:29 +0700

    Version_1

--- app/public/index.php
+++ app/public/index.php
@@ -1,23 +1,47 @@
+<html>
+
+<head></head>
+    <body>
+
+        <form action="http://localhost" method="post">
+            <div class="container">
+                <h1>Форма регистрации</h1>
+                <p>Пожалуйста, заполните эту форму, чтобы создать учетную запись.</p>
+                <hr>
+
+                <label for="name"><b>Name</b></label>
+                <input type="text" placeholder="Введите ваше имя" name="name" required>
+
+                <label for="email"><b>Email</b></label>
+                <input type="text" placeholder="Введите вашу почту" name="email" required>
+
+                 <label for="psw"><b>Пароль</b></label>
+                <input type="password" placeholder="Введите пароль" name="psw" required>
+
+                <label for="psw-repeat"><b>Повторить пароль</b></label>
+                 <input type="password" placeholder="Повторите пароль" name="psw-repeat" required>
+
+                <label>
+                    <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Запомнить меня
+                </label>
+
+                 <div class="clearfix">
+
+                        <button type="submit" class="signupbtn">Регистрация</button>
+                 </div>
+            </div>
+        </form>
+    </body>
+</html>
+
+
 <?php
 
-$method = $_SERVER['REQUEST_METHOD'];
-$uri = $_SERVER['REQUEST_URI'];
+print_r($_POST);
 
-if ($uri === '/registrate') {
-    require_once './hanlder/registrate.php';
-} elseif ($uri === '/login') {
-require_once './hanlder/login.php';
-} elseif ($uri === '/main') {
-    require_once './hanlder/main.php';
-} elseif ($uri === '/logout') {
-    session_start();
-    session_destroy();
-} else {
-    require_once './html/404.html';
-}
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['psw'];
 
-
-
-
-?>
-
+$pdo = new PDO("pgsql:host=db, dbname=dbname", "dbuser", "dbpwd");
+$pdo->exec("INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')");
